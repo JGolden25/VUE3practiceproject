5618a05830b0348c37bd4e0458482bc609a6d3c0
Joseph Chretien-Golden <joechre@gmail.com>
2020-11-13 13:50:33 -0600

    feat: add user

---
+++ twotter-vue-3/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <div id="app">
+   {{ user.username }}
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user:{
+        id: 1,
+        username: 'josephcgolden',
+        firstName: 'Joseph',
+        lastName: 'Chretien-Golden',
+        email: 'joechre@gmail.com',
+        isAdmin: true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
